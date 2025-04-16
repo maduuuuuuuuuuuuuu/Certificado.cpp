86926dbe2f4d6c3a9b5c6569b9b8486a4a42768a
Maria Eduarda Amaral <136991076+maduuuuuuuuuuuuuu@users.noreply.github.com>
2025-04-15 21:29:21 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,42 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    float nota1, nota2, nota3, frequencia;
+    
+    cout << "\n Digite sua frequencia: ";
+    cin >> frequencia;
+    
+    cout << "\n Digite a primeira nota: ";
+    cin >> nota1;
+    
+    cout << "\n Digite a segunda nota: ";
+    cin >> nota2;
+    
+    cout << "\n Digite a terceira nota: ";
+    cin >> nota3;
+    
+   
+
+
+    float media = (nota1 + nota2 + nota3) / 3;
+    bool aprovado = false;
+    
+    aprovado = (media>=7) && (frequencia>=88);
+    
+    cout << "\n frequencia: " << frequencia << "%";
+    cout << "\n media: " << media;
+    cout << "\n aprovado: " << aprovado;
+
+    
+    return 0;
